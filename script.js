8742b54c97eb640daeff5f2868fd93ea7846a570
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-06-19 15:39:45 -0500

    added notes to help guide coding based on Google research

--- script.js
+++ script.js
@@ -10,6 +10,36 @@ function writePassword() {
 
 }*/
 
+/*collect password length
+collect password requirements - upper, lower, number, symbol
+create array of upper, lower, number, symbol using ASCII codes
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+ - double check ASCII codes for each type; concatenate all types together; 
+ function arrayFromLowToHigh(low, high) {
+   const array = []
+   for (let i = low; i <= high; i++) {
+     array.push(i)
+   }
+   return
+  }
+  
+  check to see if selected
+  const includeLower = includeLowercaseEleemnt.checked (for video - how am I labeling confirms?)
+  
+  function generatePassword(characterAmount, includeUpper, includeLower, inlcudeNumb, includeSym) {
+    if (includeLower) charCodes = charCodes.concat (LOWERCASE_CHAR_CODES)
+    if (includeUpper) charCodes = charCodes.concat (UPPERCASE_CHAR_CODES)
+    etc.
+    
+    const passwordCharacter = []
+    for (let i = 0; i < characterAmount; i++) {
+      const charactersCode = charCodes[Math.floor(Math.random()) * charCodes.length)]
+      passwordCharacters.push(String.fromCharCode(characterCode))
+    }
+    return passwordCharacters.join('')
+  }
+  */
+
 function passwordLength () {
   var length = window.prompt("How many characters would you like to include in your password? Please enter a number between 8 and 128.");
   if (length >= 8 && length <= 128) {
